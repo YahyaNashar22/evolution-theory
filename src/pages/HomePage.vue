e18c5cd55b50f56f836dae5e953829eab1a288b7
YahyaNashar22 <yahyanashar22@gmail.com>
2026-07-17 16:57:03 +0300

    fix/trimmed the eerie.mp3 audio to not have blank startup

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -2,9 +2,9 @@
 import { Howl } from 'howler'
 
 import IntroComponent from '@/components/IntroComponent.vue'
-import eerieSoundUrl from '@/assets/sounds/eerie.mp3?url'
 import PlayButtonComponent from '@/components/PlayButtonComponent.vue'
 import EarthComponent from '@/components/EarthComponent.vue'
+import eerieSoundUrl from '@/assets/sounds/eerie_bg.mp3?url'
 
 const bgSound = new Howl({
   src: [eerieSoundUrl],
